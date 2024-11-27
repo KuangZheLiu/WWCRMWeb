f6cf127036a15561db3e3b5f9078fad0bfa8d3ba
Light Liu <a0935227116@gmail.com>
2024-11-27 08:49:39 +0800

    Modify code prettier

--- server/api/AnalysisData.js
+++ server/api/AnalysisData.js
@@ -3,14 +3,6 @@ const router = express.Router()
 
 const { ExecSQL } = require('./SqlPool')
 
-const query = `
-      SELECT COUNT(s01_03b) AS twtData_1 FROM InvData
-      WHERE ComNo = 'TWTTH' AND YM = '202311'
-      ORDER BY YM DESC
-      OFFSET @offset ROWS
-      FETCH NEXT @pageSize ROWS ONLY
-    `
-
 // 先 SELECT DISTINCT 找出相異
 // SELECT DISTINCT s01_03b FROM InvData where ComNo = 'TWTTH' and YM = '202402'
 
@@ -20,61 +12,6 @@ const query = `
 
 // SELECT COUNT(s01_03b) AS twtData_1 FROM InvData where ComNo = 'TWTTH' and YM = '202403' and  s01_03b = '半成品轉單客戶'
 
-async function getPagedInvData(filters = {}, page = 1, pageSize = 50) {
-  try {
-    const pool = new msSql.ConnectionPool(sqlConfig)
-    await pool.connect()
-
-    const request = pool.request()
-
-    // 構建查詢條件
-    let whereClause = '1=1'
-    if (filters.companyName) {
-      whereClause += ' AND ComNo = @companyName'
-      request.input('companyName', msSql.NVarChar, filters.companyName)
-    }
-    if (filters.time) {
-      whereClause += ' AND YM = @time'
-      request.input('time', msSql.NVarChar, filters.time)
-    }
-    if (filters.customerName) {
-      whereClause += ' AND s01b_03 = @customerName'
-      request.input('customerName', msSql.NVarChar, filters.customerName)
-    }
-
-    const offset = (page - 1) * pageSize
-
-    // 查詢數據
-    const query = `
-      SELECT
-        ComNo AS 公司名稱,
-        YM AS 時間,
-        s01b_03 AS 客戶名稱,
-        s02_02 AS 品名,
-        ShipCostMoney AS 銷貨成本,
-        ProfitRate AS 毛利率,
-        InvMoney2 AS 原幣銷貨額
-      FROM InvData
-      WHERE ${whereClause}
-      ORDER BY YM DESC
-      OFFSET @offset ROWS
-      FETCH NEXT @pageSize ROWS ONLY
-    `
-
-    request.input('offset', msSql.Int, offset)
-    request.input('pageSize', msSql.Int, pageSize)
-
-    const result = await request.query(query)
-    return result.recordset
-  } catch (err) {
-    console.error('Database query error:', err)
-    throw err
-  } finally {
-    await pool.close()
-  }
-}
-
-router.getPagedInvData = getPagedInvData
 // 1. 按公司和客戶統計每月訂單數
 router.get('/customer-analysis', async (req, res) => {
   try {
